694a08d9ea26ee0a3596ce3c178f0b77a6a549a4
MrTheHill <Harvey.hill@hotmail.co.uk>
2025-10-02 13:49:13 +0100

    restarting this project

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,29 +7,9 @@ terraform {
   }
 }
 
-variable "ProxmoxAPIKey" {
-  description = "Proxmox API Key"
-  sensitive = true
-}
-
-variable "ProxmoxAPIURL" {
-  description = "Proxmox API endpoint"
-}
-
-variable "ProxmoxUser" {
-  
-}
-
-variable "ProxmoxPass" {
-  sensitive = true
-}
-
 provider "proxmox" {
-    pm_api_url = var.ProxmoxAPIURL
-    pm_user = var.ProxmoxUser
-    pm_password = var.ProxmoxPass
-    pm_tls_insecure = true
-    pm_debug = true
+  endpoint = var.ProxmoxEndpoint
+  api_token = var.ProxmoxAPIKey
 }
 
 resource "proxmox_vm_qemu" "TestVM" {
